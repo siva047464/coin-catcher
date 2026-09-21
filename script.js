c9510ac4149f7ff06ac8b17c5dabf9daa4867d0c
Siva Kumar R <siva047464@gmail.com>
2026-09-21 20:18:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 const gameArea = document.getElementById("gameArea");
 const coin = document.getElementById("coin");
 const basket = document.getElementById("basket");
+
 const scoreDisplay = document.getElementById("score");
 const timeDisplay = document.getElementById("time");
 const levelDisplay = document.getElementById("level");
 const highScoreDisplay = document.getElementById("highScore");
-highScoreDisplay.textContent = highScore;
+
 const restartBtn = document.getElementById("restartBtn");
 const pauseBtn = document.getElementById("pauseBtn");
-let paused = false;
 
 let score = 0;
 let level = 1;
-let highScore = localStorage.getItem("highScore") || 0;
 let time = 30;
+
+let highScore = localStorage.getItem("highScore") || 0;
+
 let coinX = 220;
 let coinY = 20;
 let basketX = 220;
+
 let gameRunning = true;
+let paused = false;
+
+highScoreDisplay.textContent = highScore;
 
 function moveBasket(direction) {
     if (!gameRunning || paused) return;
@@ -51,27 +57,26 @@ function moveCoin() {
     coinY += 5 + (level - 1) * 1;
     coin.style.top = coinY + "px";
 
-    // Check if coin reaches the basket
     if (
         coinY >= 430 &&
         coinX > basketX - 20 &&
         coinX < basketX + 60
     ) {
         score++;
-scoreDisplay.textContent = score;
+        scoreDisplay.textContent = score;
 
-level = Math.floor(score / 5) + 1;
-levelDisplay.textContent = level;
+        level = Math.floor(score / 5) + 1;
+        levelDisplay.textContent = level;
 
-if (score > highScore) {
-    highScore = score;
-    localStorage.setItem("highScore", highScore);
-}
+        if (score > highScore) {
+            highScore = score;
+            localStorage.setItem("highScore", highScore);
+            highScoreDisplay.textContent = highScore;
+        }
 
-resetCoin();
+        resetCoin();
     }
 
-    // Coin missed
     if (coinY > 500) {
         resetCoin();
     }
@@ -87,13 +92,16 @@ function resetCoin() {
 
 function startTimer() {
     const timer = setInterval(function() {
+
         if (!gameRunning) {
             clearInterval(timer);
             return;
         }
+
         if (paused) {
             return;
         }
+
         time--;
         timeDisplay.textContent = time;
 
@@ -101,10 +109,27 @@ function startTimer() {
             gameRunning = false;
             clearInterval(timer);
 
+            pauseBtn.disabled = true;
+
             alert("GAME OVER! Your Score: " + score);
         }
+
     }, 1000);
 }
+
+pauseBtn.addEventListener("click", function() {
+
+    if (!gameRunning) return;
+
+    paused = !paused;
+
+    if (paused) {
+        pauseBtn.textContent = "Resume";
+    } else {
+        pauseBtn.textContent = "Pause";
+    }
+
+});
 
 restartBtn.addEventListener("click", function() {
     location.reload();
